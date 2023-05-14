a52020daceb1238c81911a934b3a0a68442583b9
SiddheshPotdar <siddheshpotdar4787@gmail.com>
2023-05-14 12:44:50 +0530

    version 1 - basic styling @ funcitonality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./Components/Footer";
+import Main from "./Pages/Main";
+import NavigationBar from "./Components/NavigationBar";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import AccidentInfo from "./Pages/AccidentInfo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavigationBar />
+			<Routes>
+				<Route path="/" element={<Main />} />
+				<Route path="/accidentInfo" element={<AccidentInfo />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
